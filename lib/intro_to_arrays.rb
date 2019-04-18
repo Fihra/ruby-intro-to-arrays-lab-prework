d8a93df2fd090eef86586d56c4fd83e99b877d4a
Fabian Fabro <fabrofabian@gmail.com>
2019-04-18 07:04:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -11,3 +11,15 @@ end
 def first_element(array)
   return array[0]
 end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[array.length -1]
+end
+
+def first_element_with_array_methods(array)
+  return array.first
+end
